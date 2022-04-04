46f1bd7979aaa08f305bc7d3a75afcd49a66bb11
Steven <stevenpez44@gmail.com>
2022-04-04 18:48:23 -0400

    Fixed all links and images for the flask formatting

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ def index() -> str:
 def about() -> str:
     return render_template("about.html")
 
+@app.route('/analyze')
+def analyze() -> str:
+    return render_template("analyze.html")
+
+@app.route('/compare')
+def compare() -> str:
+    return render_template("compare.html")
+
+@app.route('/market')
+def market() -> str:
+    return render_template("market.html")
+
+@app.route('/portfolio')
+def portfolio() -> str:
+    return render_template("portfolio.html")
+
+@app.route('/volume')
+def volume() -> str:
+    return render_template("volume.html")
+
 
 if __name__ == '__main__':
     app.run()
