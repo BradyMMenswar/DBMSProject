4ff46e7a91421e8369a22cc37fb34ef9c91603be
Steven <stevenpez44@gmail.com>
2022-04-15 09:11:19 -0400

    Passing data through python and javascript

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
@@ -13,9 +13,36 @@ def index() -> str:
 def about() -> str:
     return render_template("about.html")
 
-@app.route('/analyze')
+@app.route('/analyze', methods=['POST', 'GET'])
 def analyze() -> str:
-    return render_template("analyze.html")
+    if request.method == 'POST':
+        a=request.form
+        print(jsonify(a))
+        if request.form.get('int1'):
+            a=request.form.get('int1')
+            return render_template("analyze.html", value = [1,2])
+        if request.form.get('int2'):
+            a=request.form.get('int2')
+            return render_template("analyze.html", value = [1,2,3])
+        if request.form.get('int3'):
+            a=request.form.get('int3')
+            return render_template("analyze.html", value = [1,2,3,4])
+        if request.form.get('int4'):
+            a=request.form.get('int4')
+            return render_template("analyze.html", value = [1,2,3,4,5])
+        if request.form.get('int5'):
+            a=request.form.get('int5')
+            return render_template("analyze.html", value = [1,2,3,4,5,6,7])
+        if request.form.get('int6'):
+            a=request.form.get('int6')
+            return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
+
+        return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
+    else:
+        a=request.args.get('test')
+        print("no")
+        return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
+
 
 @app.route('/compare')
 def compare() -> str:
