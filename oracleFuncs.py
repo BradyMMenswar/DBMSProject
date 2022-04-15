ba09cacdc3e9c92d0005fb5e9388a051170f8db1
Konstantin Kerekovski <kerekovskik@ufl.edu>
2022-04-15 16:53:47 -0400

    Add function for query three

--- oracleFuncs.py
+++ oracleFuncs.py
@@ -95,6 +95,59 @@ def testQueryOne():
     end = datetime(2018, 1, 14, 0, 0, 0, 0)
     myresult = queryOne(BTC,GEMINI,start, end)
 
+def queryThree(buyerCurrencyID:int, exchangeID: int, startDate: datetime, endDate: datetime) -> queryResult:
+    DEBUG = config('DEBUG', default=False, cast=bool)
+    SQL = """
+        WITH
+        btcprice_bitstamp (TRADE_DATE, Bitcoin_Price) AS
+        (SELECT TRADE_DATE,OPEN AS Bitcoin_Price
+        FROM  KEREKOVSKIK.OHLC_DATA_POINT
+        WHERE BUYER_CURRENCY = 1 AND EXCHANGE_ID = :1
+        ORDER BY TRADE_DATE ASC),
+        ethprice_bitstamp (TRADE_DATE, Ethereum_Price) AS
+        (SELECT TRADE_DATE,OPEN AS Ethereum_Price
+        FROM  KEREKOVSKIK.OHLC_DATA_POINT
+        WHERE BUYER_CURRENCY = 2 AND EXCHANGE_ID = :2
+        ORDER BY TRADE_DATE ASC)
+        SELECT btcprice_bitstamp.trade_date,Bitcoin_Price,Ethereum_Price,(Bitcoin_Price + Ethereum_Price)/2 AS Market_Price
+        FROM btcprice_bitstamp,ethprice_bitstamp
+        WHERE btcprice_bitstamp.trade_date = ethprice_bitstamp.trade_date
+        AND btcprice_bitstamp.trade_date between :3 and :4 
+        ORDER BY trade_date ASC
+    """
+
+    result: queryResult = queryResult()
+
+    with getConnection() as connection:
+        with connection.cursor() as cursor:
+            resultSet = cursor.execute(SQL,(exchangeID,exchangeID,startDate,endDate))
+            for row in resultSet:
+                result.A.append(row[0])
+
+
+                
+                if buyerCurrencyID == 1: #Return Bitcoin_Price if buyerCurrencyID==1 (BTC)
+                    result.B.append(row[1])
+                else: # Return Ethereum_Price if buyerCurrencyID==2 (ETH)
+                    result.B.append(row[2])
+                
+                result.C.append(row[3])
+                #print(row)
+
+            cursorRowCount=cursor.rowcount
+    if DEBUG:
+        print("### buyerCurrencyID: {} exchangeID: {} startDate: {} endDate: {} RowCount: {} CursorRowCount: {} ".format(buyerCurrencyID,exchangeID,startDate,endDate,len(result.A),cursorRowCount ))
+    return result
+
+
+def testQueryThree():
+
+    # datetime(year, month, day, hour, minute, second, microsecond)
+    start = datetime(2018, 1, 1, 0, 0, 0, 0)
+    end = datetime(2018, 1, 14, 0, 0, 0, 0)
+    myresult = queryThree(ETH,GEMINI,start, end)
+    print(myresult)
+
 def queryFour(buyerCurrencyID:int, exchangeID: int, startDate: datetime, endDate: datetime) -> queryResult:
 
     SQL = """
@@ -129,4 +182,5 @@ def testQueryFour():
 
 if __name__ == "__main__":
     #testQueryFour()
+    #testQueryThree()
     testConnection()
