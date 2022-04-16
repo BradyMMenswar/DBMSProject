405a886271e5821c27ad6144b4a5cfc8df2d616b
BradyMMenswar <bradymenswar@ufl.edu>
2022-04-16 17:30:24 -0400

    Added Query Two Chart
    Portfolio calculator is functional.
    Changed some css to accommodate new chart.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
-from oracleFuncs import queryOne, queryFour, queryThree
+from oracleFuncs import queryOne, queryFour, queryThree, queryTwo
 import math
 
 app = Flask(__name__, static_url_path='',
@@ -46,25 +46,6 @@ def analyze() -> str:
             # print(len(chartForm[4]))
             # print(len(chartForm[5]))
             return render_template("analyze.html", value = chartForm)
-        
-        # if request.form.get('int1'):
-        #     a=request.form.get('int1')
-        #     return render_template("analyze.html", value = [1,2])
-        # if request.form.get('int2'):
-        #     a=request.form.get('int2')
-        #     return render_template("analyze.html", value = [1,2,3])
-        # if request.form.get('int3'):
-        #     a=request.form.get('int3')
-        #     return render_template("analyze.html", value = [1,2,3,4])
-        # if request.form.get('int4'):
-        #     a=request.form.get('int4')
-        #     return render_template("analyze.html", value = [1,2,3,4,5])
-        # if request.form.get('int5'):
-        #     a=request.form.get('int5')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7])
-        # if request.form.get('int6'):
-        #     a=request.form.get('int6')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
         query = 0;
         return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
     else:
@@ -112,25 +93,6 @@ def market() -> str:
             # print(len(chartForm[4]))
             # print(len(chartForm[5]))
             return render_template("market.html", value = chartForm)
-        
-        # if request.form.get('int1'):
-        #     a=request.form.get('int1')
-        #     return render_template("analyze.html", value = [1,2])
-        # if request.form.get('int2'):
-        #     a=request.form.get('int2')
-        #     return render_template("analyze.html", value = [1,2,3])
-        # if request.form.get('int3'):
-        #     a=request.form.get('int3')
-        #     return render_template("analyze.html", value = [1,2,3,4])
-        # if request.form.get('int4'):
-        #     a=request.form.get('int4')
-        #     return render_template("analyze.html", value = [1,2,3,4,5])
-        # if request.form.get('int5'):
-        #     a=request.form.get('int5')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7])
-        # if request.form.get('int6'):
-        #     a=request.form.get('int6')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
         query = 0
         return render_template("market.html", value = [1,2,3,4,5,6,7,8,9,10])
     else:
@@ -138,9 +100,44 @@ def market() -> str:
         print("no")
         return render_template("market.html", value = [1,2,3,4,5,6,7,8,9,10])
 
-@app.route('/portfolio')
+@app.route('/portfolio', methods=['POST', 'GET'])
 def portfolio() -> str:
-    return render_template("portfolio.html")
+    if request.method == 'POST':
+        chartForm = [0, 0, 0, 0, 0, 0, 0]
+        if(request.form.get('exchange-data')):
+            chartForm[0] = request.form.get('exchange-data')
+            chartForm[1] = str(datetime.strptime(request.form.get('start-data'), '%Y-%m-%dT%H:%M'))
+            chartForm[2] = str(datetime.strptime(request.form.get('end-data'), '%Y-%m-%dT%H:%M'))
+            chartForm[3] = request.form.get('ETHAmount')
+            chartForm[4] = request.form.get('BTCAmount')
+            chartForm[5] = []
+            chartForm[6] = []
+            query = queryTwo(request.form.get('exchange-data'), datetime.strptime(request.form.get('start-data'), '%Y-%m-%dT%H:%M'), datetime.strptime(request.form.get('end-data'), '%Y-%m-%dT%H:%M'), chartForm[3], chartForm[4])
+            queryA = query.A
+            queryALength = len(queryA)
+            scaleFactor = math.floor(queryALength / 100)
+            for x in range(queryALength):
+                if(x % scaleFactor == 0):
+                    chartForm[5].append(str(queryA[x]))
+            # for d in queryA:
+            #     chartForm[4].append(str(d))
+            queryB = query.B
+            queryC = query.C
+            for y in range(queryALength):
+                if(y % scaleFactor == 0):
+                    chartForm[6].append(queryB[y] + queryC[y])
+            # for p in queryC:
+            #     chartForm[5].append(p)
+            print(chartForm)
+            # print(len(chartForm[4]))
+            # print(len(chartForm[5]))
+            return render_template("portfolio.html", value = chartForm)
+        query = 0;
+        return render_template("portfolio.html", value = [1,2,3,4,5,6,7,8,9,10])
+    else:
+        a=request.args.get('test')
+        print("no")
+        return render_template("portfolio.html", value = [1,2,3,4,5,6,7,8,9,10])
 
 @app.route('/volume', methods=['POST', 'GET'])
 def volume() -> str:
@@ -172,25 +169,6 @@ def volume() -> str:
             # print(len(chartForm[4]))
             # print(len(chartForm[5]))
             return render_template("volume.html", value = chartForm)
-        
-        # if request.form.get('int1'):
-        #     a=request.form.get('int1')
-        #     return render_template("analyze.html", value = [1,2])
-        # if request.form.get('int2'):
-        #     a=request.form.get('int2')
-        #     return render_template("analyze.html", value = [1,2,3])
-        # if request.form.get('int3'):
-        #     a=request.form.get('int3')
-        #     return render_template("analyze.html", value = [1,2,3,4])
-        # if request.form.get('int4'):
-        #     a=request.form.get('int4')
-        #     return render_template("analyze.html", value = [1,2,3,4,5])
-        # if request.form.get('int5'):
-        #     a=request.form.get('int5')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7])
-        # if request.form.get('int6'):
-        #     a=request.form.get('int6')
-        #     return render_template("analyze.html", value = [1,2,3,4,5,6,7,8,9,10])
         query = 0;
         return render_template("volume.html", value = [1,2,3,4,5,6,7,8,9,10])
     else:
